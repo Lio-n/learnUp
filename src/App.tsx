2b349ab4fe459b984f97dfaebeee515699b06162
Santiago <san_chan97@hotmail.com>
2024-10-02 20:21:33 -0300

    header completo

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Header className="header"></Header>
+      <Header></Header>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/ui/molecules/Header.tsx
+++ src/ui/molecules/Header.tsx
@@ -1,45 +1,22 @@
 import owlLogo from "../../assets/owl-white.png";
 import menu from "../../assets/ham-menu.svg";
 
-type Variant = "default" | "illuminated";
-type Color = "bg-eerie-black" | "bg-lavender-indigo" | "bg-violet-blue";
-
-function Header({
-  variant = "default" as Variant,
-  color = "bg-eerie-black" as Color,
-  className = "",
-}) {
-  // Clases dinámicas según las props
-  const colorClasses = {
-    "bg-eerie-black": {
-      default: "bg-eerie-black",
-      illuminated: "bg-eerie-black animate-bubbleMoveBlack",
-    },
-    "bg-lavender-indigo": {
-      default: "bg-lavender-indigo",
-      illuminated: "bg-lavender-indigo animate-bubbleMoveLavenderIndigo",
-    },
-    "bg-violet-blue": {
-      default: "bg-violet-blue",
-      illuminated: "bg-violet-blue animate-bubbleMoveVioletBlue",
-    },
-  };
-
-  const selectedColorClass = colorClasses[color][variant];
-
-  // Combinación de clases
-  // ! Tener en cuenta que las clases manuales sean las mismas
-  // ! que se usan en el componente para tengan efecto en la especificidad
-  const classes = [
-    selectedColorClass,
-    className, // Agregar className al final para que tenga prioridad
-  ]
-    .filter(Boolean) // Filtrar valores falsy
-    .join(" "); // Unir las clases en una cadena
+function Header({}) {
   return (
-    <header className={classes}>
-      <img src={menu}></img>
-      <img className="header-logo" src={owlLogo} alt="" />
+    <header className="bg-eerie-black flex justify-between md:justify-center p-2.5 max-h-20">
+      <img className="block md:hidden header-logo" src={menu} alt="menu" />
+      <img className="block md:hidden header-logo" src={owlLogo} alt="logo" />
+      <nav className="hidden md:flex md:w-[55%] lg:flex lg:w-[55%] justify-around items-center">
+        <a href="">Home</a>
+        <a href="">How it works</a>
+        <img
+          className="header-logo w-16 h-16 object-contain"
+          src={owlLogo}
+          alt=""
+        />
+        <a href="">FAQs</a>
+        <a href="">Contact</a>
+      </nav>
     </header>
   );
 }
