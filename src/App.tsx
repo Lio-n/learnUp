85763c2321ae075e8b80d53fa32a237127865ea3
Lio-n <leonardofontan14@gmail.com>
2024-10-04 16:25:03 -0300

    feat : add FAQs section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './App.css';
 import Home from './ui/organisms/home.organism';
 import Header from './ui/molecules/Header';
 import { HowItWorks } from './ui/organisms/how-it-works.organism';
+import { FAQs } from './ui/organisms/FAQs.organism';
 
 function App() {
   return (
     <>
       <Header></Header>
       <Home />
+      <div className="py-20" />
       <HowItWorks />
+      <div className="py-20" />
+      <FAQs />
+      <div className="py-20" />
     </>
   );
 }
